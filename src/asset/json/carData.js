a492297a06cf01ac8a700e59e98b2e1df3ed8a4b
Programmer-Mehadi <mehadi.developer@gmail.com>
2023-01-20 23:25:12 +0600

    project setup.

---
+++ src/asset/json/carData.js
@@ -0,0 +1,50 @@
+[
+    {
+        "id": "1",
+        "name": "Hyundai Grand i10",
+        "img": "https://stimg.cardekho.com/images/carexteriorimages/930x620/Hyundai/Grand-i10/7715/1585572310544/front-left-side-47.jpg",
+        "color": "White",
+        "currentBooking": "false",
+        "seatingCapacity": "4",
+        "vehicleNumber": "18 D 4356",
+        "description": "The Hyundai Grand i10 has 1 Diesel Engine, 1 Petrol Engine and 1 CNG Engine on offer. The Diesel engine is 1186 cc, the Petrol engine is 1197 cc while the CNG engine is 1197 cc . It is available with Manual & Automatic transmission.Depending upon the variant and fuel type the Grand i10 has a mileage of 17.0 to 24.0 kmpl & Ground clearance of Grand i10 is 165mm. The Grand i10 is a 5 seater 4 cylinder car and has length of 3765mm, width of 1660mm and a wheelbase of 2425mm.",
+        "rentPerDay": "350",
+        "fuelType": "Pertorl"
+    },
+    {
+        "id": "2",
+        "name": "Ford EcoSport",
+        "img": "https://cdn.motor1.com/images/mgl/KKMG0/s1/ford-ecosport-storm-4wd.jpg",
+        "color": "Deep Blue",
+        "currentBooking": "false",
+        "seatingCapacity": "4",
+        "vehicleNumber": "21 F 45V",
+        "description": "The Ford Ecosport 2015-2021 has 1 Diesel Engine and 4 Petrol Engine on offer. The Diesel engine is 1498 cc while the Petrol engine is 1496 cc and 1497 cc and 1499 cc and 999 cc . It is available with Manual & Automatic transmission.Depending upon the variant and fuel type the Ecosport 2015-2021 has a mileage of 23.0 kmpl & Ground clearance of Ecosport 2015-2021 is 200mm. The Ecosport 2015-2021 is a 5 seater 3 cylinder car and has length of 3998mm, width of 1765mm and a wheelbase of 2519mm.",
+        "rentPerDay": "1350",
+        "fuelType": "Petrol"
+    },
+    {
+        "id": "3",
+        "name": "Maruti Suzuki XL6",
+        "img": "https://www.indiacarnews.com/wp-content/uploads/2019/08/Maruti-XL6-Details.jpg",
+        "color": "Glossy Black",
+        "currentBooking": "true",
+        "seatingCapacity": "6",
+        "vehicleNumber": "M 34 XL6",
+        "description": "Maruti Suzuki has launched an all-new sportier version of the standard Ertiga MPV in India. Called the Maruti XL6, the new model is available in the price range of Rs 9.79 lakh to Rs 11.46 lakh (ex-showroom). The new model is sold exclusively through the NEXA premium sales and service network. In the company’s product line-up, the Maruti Suzuki XL6 is positioned above the S-Cross.The Maruti XL6 cross comes equipped with significant exterior and interior changes. Majority of the changes has been made at the front end of the car. It is a six-seater model with captain seats for all 3 rows. The XL6 cross is powered by a BSVI compliant 1.5-litre K15B petrol engine that also powers the regular model. Transmission options include a 5-speed manual and a 4-speed torque convertor automatic.",
+        "rentPerDay": "850",
+        "fuelType": "Petrol and CNG"
+    },
+    {
+        "id": "4",
+        "name": "Honda CR -V",
+        "img": "https://images.hgmsites.net/lrg/honda-cr-z_100532391_l.jpg",
+        "color": "Blue",
+        "currentBooking": "false",
+        "seatingCapacity": "4",
+        "vehicleNumber": "H 45 CRV",
+        "description": "Power is provided by a supercharged 1.5-liter engine that produces 156 HP (115 kW) and 185 Nm (136 lb-ft) of torque. It is backed up by an IMA hybrid system that develops 20 HP (15 kW) and 78 Nm (58 lb-ft) of torque.The top speed of the CR-Z wasn't dizzying, but 124 mph showed it wasn't exactly a slouch either. The punch the hybrid system gave as you put your foot down was quite impressive as well, although its 0-62 mph time was a little slow at 9.9 seconds.",
+        "rentPerDay": "450",
+        "fuelType": "Regular Gasoline"
+    }
+]
